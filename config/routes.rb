4de4777cc5b95b6511c33c1f00437445d548d661
プログラミング <programming@Shinsukes-MacBook-Pro.local>
2016-01-31 18:27:11 +0900

    リソースベースに書き換えました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
-  get 'index' => 'home#index'
-  get 'show' => 'products#show'
-  post 'create/:id' => 'reviews#create'
-  delete 'destroy/:id' => 'reviews#destroy'
-  get 'show/:id' => 'users#show'
-  get 'edit/:id' => 'users#edit'
-  patch 'update/:id' => 'users#update'
+  resoureces :application
+  resources :home
+  resources :products
+  resources :reviews
+  resources :users
 end
