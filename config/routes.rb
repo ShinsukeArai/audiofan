40fdcc6f0fef893d368d2205daad55c305272db2
プログラミング <programming@Shinsukes-MacBook-Pro.local>
2016-02-15 16:25:57 +0900

    devise_for :usersを書き足しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :home, only: :index
   resources :products, only: :show do
     resources :reviews, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :update]
   root 'home#index'
 end
